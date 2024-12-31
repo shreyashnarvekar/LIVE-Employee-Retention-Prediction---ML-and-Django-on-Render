ac48c41910fc8f791dbf4a76548c288526f5c87b
Shreyash Narvekar <dev.shreyashnarvekar@gmail.com>
2025-01-01 03:00:31 +0530

    Add .sav for git release

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect 
 from datetime import datetime
 import pickle
+import os
+import zipfile
+import urllib.request
 
 # Create your views here. 
 def home(request): 
@@ -8,9 +11,32 @@ def home(request):
     return render(request, 'index.html', {'range': range(1, 337), 'current_year': current_year})
 
 def getPredictions(city, cdi, gender, relevent_experience, enrolled_university, education_level, major_discipline, experience, company_size, company_type, last_new_job, training_hours):
-    model = pickle.load(open('home/templates/ml_model.sav', 'rb'))
-    scaled = pickle.load(open('home/templates/scaler.sav', 'rb'))
+
+    # URL to your GitHub release (replace with your actual release link)
+    url = "https://github.com/user-attachments/files/18283719/ml_model.zip"
     
+    # Path to save the downloaded file
+    zip_path = "/tmp/model_files.zip"
+
+    # Download the ZIP file
+    urllib.request.urlretrieve(url, zip_path)
+
+    # Extract the ZIP file
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall("/tmp/")
+
+    # Load the model and scaler from the extracted files
+    model_path = "/tmp/ml_model.sav"
+    scaler_path = "/tmp/scaler.sav"
+    
+    # Load the model and scaler using pickle
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
+    
+    with open(scaler_path, 'rb') as scaler_file:
+        scaled = pickle.load(scaler_file)
+    
+
     prediction = model.predict(scaled.transform([
         [city, cdi, gender, relevent_experience, enrolled_university, education_level, major_discipline, experience, company_size, company_type, last_new_job, training_hours]
     ]))
